882be76ddea7aaaf2bcd1ece37d16e93c8ef5f4f
Ruisics <36693911+Ruisics@users.noreply.github.com>
2018-11-02 22:04:50 +0000

    #2

---
+++ app/src/main/java/com/rp/databasedemo/ArtistsList.java
@@ -0,0 +1,45 @@
+package com.rp.databasedemo;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ArtistsList extends ArrayAdapter<Artist> {
+
+    private Activity context;
+    private List<Artist> artistList;
+
+    public ArtistsList(Activity context, List<Artist> artistList){
+
+        super(context, R.layout.list_layout, artistList);
+        this.context = context;
+        this.artistList = artistList;
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = context.getLayoutInflater();
+
+        View listViewItem = inflater.inflate(R.layout.list_layout, null, true);
+
+        TextView textViewName = listViewItem.findViewById(R.id.textViewName);
+        TextView textViewGenre = listViewItem.findViewById(R.id.textViewGenre);
+
+        Artist artist = artistList.get(position);
+
+        textViewName.setText(artist.getArtistName());
+        textViewGenre.setText(artist.getArtistGenre());
+
+        return listViewItem;
+
+    }
+}
--- app/src/main/java/com/rp/databasedemo/MainActivity.java
+++ app/src/main/java/com/rp/databasedemo/MainActivity.java
@@ -1,16 +1,25 @@
 package com.rp.databasedemo;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
     Spinner spinnerGenres;
 
     DatabaseReference databaseArtists;
+
+    ListView listViewArtists;
+
+    List<Artist> artistList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +44,46 @@ public class MainActivity extends AppCompatActivity {
         btnAddArtist = findViewById(R.id.btnAddArtist);
         spinnerGenres = findViewById(R.id.spinnerGenres);
 
+        listViewArtists = findViewById(R.id.listViewArtists);
+
+        artistList = new ArrayList<>();
+
         btnAddArtist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 addArtist();
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        databaseArtists.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                artistList.clear();
+
+                for(DataSnapshot artistSnapshot : dataSnapshot.getChildren()){
+
+                    Artist artist = artistSnapshot.getValue(Artist.class);
+
+                    artistList.add(artist);
+
+                }
+
+                ArtistsList adapter = new ArtistsList(MainActivity.this, artistList);
+                listViewArtists.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });

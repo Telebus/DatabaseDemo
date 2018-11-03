456318996a983fd37422370413da4b2f9ef174e7
Rúben Príncipe <36694769+M4Timbola29@users.noreply.github.com>
2018-11-03 15:42:14 +0000

    #4

--- app/src/main/java/com/rp/databasedemo/AddTrackActivity.java
+++ app/src/main/java/com/rp/databasedemo/AddTrackActivity.java
@@ -1,6 +1,7 @@
 package com.rp.databasedemo;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -12,11 +13,15 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AddTrackActivity extends AppCompatActivity {
@@ -30,6 +35,9 @@ public class AddTrackActivity extends AppCompatActivity {
     ListView listViewTracks;
 
     DatabaseReference databaseTracks;
+
+
+    List<Track> tracks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +54,7 @@ public class AddTrackActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
 
+        tracks = new ArrayList<>();
         String id = intent.getStringExtra(MainActivity.ARTIST_ID);
         String name = intent.getStringExtra(MainActivity.ARTIST_NAME);
 
@@ -61,6 +70,35 @@ public class AddTrackActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        databaseTracks.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                tracks.clear();
+
+                for (DataSnapshot trackSnapshot : dataSnapshot.getChildren()){
+
+                    Track track = trackSnapshot.getValue(Track.class);
+                    tracks.add(track);
+
+                }
+                TrackList trackListAdapter = new TrackList(AddTrackActivity.this, tracks);
+                listViewTracks.setAdapter(trackListAdapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     private void saveTrack(){
---
+++ app/src/main/java/com/rp/databasedemo/TrackList.java
@@ -0,0 +1,46 @@
+package com.rp.databasedemo;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class TrackList extends ArrayAdapter<Track> {
+
+    private Activity context;
+    private List<Track> tracksList;
+
+    public TrackList(Activity context, List<Track> tracksList){
+
+        super(context, R.layout.layout_track_list, tracksList);
+        this.context = context;
+        this.tracksList = tracksList;
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = context.getLayoutInflater();
+
+        View listViewItem = inflater.inflate(R.layout.list_layout, null, true);
+
+        TextView textViewName = listViewItem.findViewById(R.id.textViewName);
+        TextView textViewRating = listViewItem.findViewById(R.id.textViewRating);
+
+        Track track = tracksList.get(position);
+
+        textViewName.setText(track.getTrackName());
+        textViewRating.setText(String.valueOf(track.getTrackRating()));
+
+        return listViewItem;
+
+    }
+
+}
